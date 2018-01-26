17f63fd6a88c2c60616332c9c10da24174e180ab
manupstairs <manupstairs@163.com>
2018-01-26 15:34:35 +0800

    add form

---
+++ SocketClientForm/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SocketClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxReceive = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(36, 249);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(391, 240);
+            this.txtSend.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Send Message";
+            // 
+            // listBoxReceive
+            // 
+            this.listBoxReceive.FormattingEnabled = true;
+            this.listBoxReceive.ItemHeight = 24;
+            this.listBoxReceive.Location = new System.Drawing.Point(630, 249);
+            this.listBoxReceive.Name = "listBoxReceive";
+            this.listBoxReceive.Size = new System.Drawing.Size(567, 220);
+            this.listBoxReceive.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 676);
+            this.Controls.Add(this.listBoxReceive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxReceive;
+    }
+}
+
---
+++ SocketClientForm/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketClientForm
+{
+    public partial class Form1 : Form
+    {
+        private Socket socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += FormLoad;
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.8.105");
+            IPEndPoint point = new IPEndPoint(ip, 2333);
+            //进行连接
+            socketClient.Connect(point);
+
+            //不停的接收服务器端发送的消息
+            Thread thread = new Thread(Recive);
+            thread.IsBackground = true;
+            thread.Start(socketClient);
+        }
+
+        private void Recive(object o)
+        {
+            var send = o as Socket;
+            while (true)
+            {
+                //获取发送过来的消息
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                var effective = send.Receive(buffer);
+                if (effective == 0)
+                {
+                    break;
+                }
+                var str = Encoding.UTF8.GetString(buffer, 0, effective);
+                //Console.WriteLine(str);
+                this.listBoxReceive.Items.Add(str);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var input = this.txtSend.Text.Trim();
+            var buffter = Encoding.UTF8.GetBytes(input);
+            socketClient.Send(buffter);
+        }
+    }
+}
